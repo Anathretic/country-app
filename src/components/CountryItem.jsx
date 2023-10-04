5d7ea5d63866becfedd8c89db6d70c726e6517a7
Konrad <konrad.wojtylo.9@gmail.com>
2023-10-04 15:47:49 +0200

    Added search input and select with whole logic, and created a dark mode button

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,7 +1,7 @@
 export const CountryItem = ({ data, moreInfoHandler }) => {
 	return (
 		<div>
-			<p>{data.name.official}</p>
+			<p>{data.name.common}</p>
 			<button onClick={() => moreInfoHandler(data.cca3)}>More Info</button>
 		</div>
 	)
--- src/components/CountryMoreInfo.jsx
+++ src/components/CountryMoreInfo.jsx
@@ -1,12 +1,14 @@
-export const CountryMoreInfo = ({ data, setShowMoreInfo }) => {
+export const CountryMoreInfo = ({ data, setShowMoreInfo, setInputValue, setSelectValue }) => {
 	const handleBack = () => {
 		setShowMoreInfo(false)
+		setInputValue('')
+		setSelectValue('')
 	}
 
 	console.log(data)
 	return (
 		<div>
-			<p>{data[0].name.common}</p>
+			<p>{data[0].name.official}</p>
 			<button onClick={handleBack}>Back</button>
 		</div>
 	)
